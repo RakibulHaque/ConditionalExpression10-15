145536b95056172b0c58ff37006df9cbf9c33381
RakibulHaque <hrakibul@sun21.co.jp>
2019-10-15 17:49:19 +0900

    commit

---
+++ src/TernaryOperator.java
@@ -0,0 +1,11 @@
+
+
+public class TernaryOperator {
+
+	public static void main(String[] args)
+	{
+		Object ternaryCondition1 = true? new Integer(4):new Float(2.0); 
+		
+		System.out.println(ternaryCondition1);
+	}
+}
